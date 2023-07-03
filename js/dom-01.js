5a987e3561512730ca245039e115c96dc7f9bc41
Bom <dechakomin290@gmail.com>
2023-07-03 18:09:32 +0700

    Updates

---
+++ js/dom-01.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const inputElements = [
+        ...document.querySelectorAll('input[type="number"].app-cmp-input'),
+    ];
+
+    inputElements.forEach((element) => {
+        element.addEventListener('change', () => {
+        const result = inputElements.reduce(
+        (carry , elem) => carry + elem.valueAsNumber,
+        0,
+        );   
+
+            document.querySelector(
+                'output.app-cmp-result',
+            ).value = `${result.toLocaleString()}`;
+        });
+    });
+});
+
+
+/*
+            let result = 0;
+            for(const elem of inputElements){
+                result += elem.valueAsNumber;
+            }
+*/
